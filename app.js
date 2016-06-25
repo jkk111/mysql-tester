1313d4d9a89e3747622c745f0838fb66cfb061ed
John Kelly <kellyj58@tcd.ie>
2016-06-25 21:16:37 +0100

    First commit of mysql-tester

---
+++ app.js
@@ -0,0 +1,2 @@
+var http = require("http");
+http.createServer(require("./index.js")).listen(80);
---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+var multer = require("multer");
+var mysql = require("mysql2");
+var conn = mysql.createConnection({
+  user: "root",
+  password: "root"
+});
+conn.connect();
+app.use(multer().single());
+app.use(express.static("static"));
+
+app.post("/sql", function(req, res) {
+  var sql = req.body.sql.replace(/\\r\\n/, "\n");
+  console.log(req.body);
+  if(!sql) return res.status(400).send("no");
+  conn.query(sql, function(err, results, data) {
+    if(err) return res.status(500).send(err);
+    res.send({data: data, results: results});
+  });
+});
+module.exports = app;
